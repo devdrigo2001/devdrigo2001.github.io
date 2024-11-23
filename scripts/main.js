694b9e84941bf232cdadb90da499e7ec90cacd4a
devdrigo01 <blackdragonmice2@gmail.com>
2024-11-22 21:55:52 -0300

    update perfil section and add footer

--- scripts/main.js
+++ scripts/main.js
@@ -7,8 +7,11 @@ perfil_section = document.querySelector("#perfil");
 menu_perfil.addEventListener('click',() =>{
     if(perfil_section.style.display ==="flex"){
         perfil_section.style.display = "none";
+       
     }else{
         perfil_section.style.display = "flex";
+        
+        
     }
 
 
